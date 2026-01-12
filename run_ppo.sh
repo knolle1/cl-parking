16ebbcb4b5d9392ff08ee4f0c7065fc9dd1694dd
nollek <nollek@tcd.ie>
2026-01-12 14:47:09 +0000

    Add experiment descriptions

--- run_ppo.sh
+++ run_ppo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python experiment.py -a ppo -s perpendicular -p results/ppo/perpendicular -n 0 --gpu 0
-python experiment.py -a ppo -s diagonal-25 -p results/ppo/diagonal-25 -n 0 --gpu 0
-python experiment.py -a ppo -s diagonal-50 -p results/ppo/diagonal-50 -n 0 --gpu 0
-python experiment.py -a ppo -s parallel -p results/ppo/parallel -n 0 --gpu 0
-python experiment.py -a ppo -s parallel-adj -p results/ppo/parallel-adj -n 0 --gpu 0
-python experiment.py -a ppo -s interleave -p results/ppo/interleave -n 0 --gpu 0
-python experiment.py -a ppo -s sequential-inc -p results/ppo/sequential-inc -n 5 --gpu 0 --start 1
-python experiment.py -a ppo -s sequential-dec -p results/ppo/sequential-dec -n 5 --gpu 0
+python experiment.py -a ppo -s perpendicular -p results/ppo/perpendicular -d "PPO trained in perpendicular parking scenario" -n 5 --gpu 0 &
+python experiment.py -a ppo -s diagonal-25 -p results/ppo/diagonal-25 -d "PPO trained in 25 degree diagonal parking scenario" -n 5 --gpu 0 &
+python experiment.py -a ppo -s diagonal-50 -p results/ppo/diagonal-50 -d "PPO trained in 50 degree diagonal parking scenario" -n 5 --gpu 0 &
+python experiment.py -a ppo -s parallel -p results/ppo/parallel -d "PPO trained in parallel parking scenario" -n 5 --gpu 0 &
+python experiment.py -a ppo -s parallel-adj -p results/ppo/parallel-adj -d "PPO trained in parallel parking scenario with adjusted reward function" -n 5 --gpu 0 &
+python experiment.py -a ppo -s interleave -p results/ppo/interleave -d "PPO trained in interleaved parking scenario" -n 5 --gpu 0 &
+python experiment.py -a ppo -s sequential-inc -p results/ppo/sequential-inc -d "PPO trained in sequential parking scenarios (perpendicular -> 25 degree -> 50 degree -> parallel)" -n 5 --gpu 0 &
+python experiment.py -a ppo -s sequential-dec -p results/ppo/sequential-dec -d "PPO trained in sequential parking scenarios (parallel -> 50 degree -> 25 degree -> perpendicular)" -n 5 --gpu 0
--- run_sac.sh
+++ run_sac.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#python experiment.py -a sac -s perpendicular -p results/sac/perpendicular -n 0 --gpu 0
-#python experiment.py -a sac -s diagonal-25 -p results/sac/diagonal-25 -n 0 --gpu 0
-#python experiment.py -a sac -s diagonal-50 -p results/sac/diagonal-50 -n 0 --gpu 0
-#python experiment.py -a sac -s parallel -p results/sac/parallel -n 0 --gpu 0
-#python experiment.py -a sac -s interleave -p results/sac/interleave -n 5 --gpu 0 --start 4
-python experiment.py -a sac -s sequential-inc -p results/sac/sequential-inc -n 5 --gpu 1 --start 2
-python experiment.py -a sac -s sequential-dec -p results/sac/sequential-dec -n 5 --gpu 1
+python experiment.py -a sac -s perpendicular -p results/sac/perpendicular -d "SAC trained in perpendicular parking scenario" -n 5 --gpu 0 &
+python experiment.py -a sac -s diagonal-25 -p results/sac/diagonal-25 -d "SAC trained in 25 degree diagonal parking scenario" -n 5 --gpu 0 &
+python experiment.py -a sac -s diagonal-50 -p results/sac/diagonal-50 -d "SAC trained in 50 degree diagonal parking scenario" -n 5 --gpu 0 &
+python experiment.py -a sac -s parallel -p results/sac/parallel -d "SAC trained in parallel parking scenario" -n 5 --gpu 0 &
+python experiment.py -a sac -s interleave -p results/sac/interleave -d "SAC trained in interleaved parking scenarios" -n 5 --gpu 0  &
+python experiment.py -a sac -s sequential-inc -p results/sac/sequential-inc -d "SAC trained in sequential parking scenarios (perpendicular -> 25 degree -> 50 degree -> parallel)" -n 5 --gpu 0
+python experiment.py -a sac -s sequential-dec -p results/sac/sequential-dec -d "SAC trained in sequential parking scenarios (parallel -> 50 degree -> 25 degree -> perpendicular)" -n 5 --gpu 0
